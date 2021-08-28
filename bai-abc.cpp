cfe885fdf3d116e581000bf56ed78c9707a60381
PCN <phamcongnhat1415@gmail.com>
2021-08-29 03:58:31 +0700

    sua file bai-abc.cpp

--- bai-abc.cpp
+++ bai-abc.cpp
@@ -1,1 +1,34 @@
-sfhaskjfhskfhskf
+/* // 1 �IEU KIEN
+#include "stdio.h"
+int main (){
+	//Nhap so n, kiem tra n nho hon 20, neu co thi thong bao ra m�n hinh
+	int n; 
+	printf("n=");
+	scanf("%d",&n);
+ 
+ 	if(n<20){
+	 printf("\n So nhap vao nho hon 20");
+	 }
+	 	
+	}
+	*/ 
+// 2 �IEU KIEN 
+#include "stdio.h"
+int main (){
+	// Nhap so n, xuat ra man hinh b la so CHAN hay LE 
+	int n;
+	printf("n=");
+	scanf("%d",&n);
+	 
+	
+	if (99>n>9){
+	
+		printf("\n N la so co 2 chu so");
+		}else if (n%2==1){
+			printf("\n n a la so le co 2 chu so ");}
+		else { printf ("\n n la so CHAN co 2 chu so");}
+	 }
+	
+	
+	
+	
